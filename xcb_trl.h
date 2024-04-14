2c49a2b26fa4dfeea82192bcaf1bffe70cb011ed
Joseph <119084558+DerjenigeUberMensch@users.noreply.github.com>
2024-04-14 23:25:23 +0000

    Removed semicolon in XCB_NONE macro (#19)

--- xcb_trl.h
+++ xcb_trl.h
@@ -479,7 +479,7 @@ enum XCBWindowState
  * Except for GX for some reason?
  */
 
- #define XCBNone               XCB_NONE;
+ #define XCBNone               XCB_NONE
 
  /***************************************************************** 
  * EVENT DEFINITIONS 
@@ -2062,6 +2062,7 @@ char *
 XCBGetErrorMinorCodeText(
         uint16_t minor_code
         );
+
 /* Returns (The full error) using a number provided.
  * The different vs just XCBGetErrorText is you get the server dying mini description.
  * EX: BadMatch (invalid parameter attributes).
